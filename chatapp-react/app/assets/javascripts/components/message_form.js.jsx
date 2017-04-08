895367353c2b1ab72bbbd77f44e5cc331b0c1f52
atsu <atsu9615@me.com>
2017-04-08 15:30:33 +0900

    簡易チャットアプリが作れるように

---
+++ chatapp-react/app/assets/javascripts/components/message_form.js.jsx
@@ -0,0 +1,26 @@
+var MessageForm = React.createClass({
+  handleSubmit: function (event) {
+    console.log('handleSubmit called!');
+    event.preventDefault();
+    var user = this.refs.user.value.trim();
+    var text = this.refs.text.value.trim();
+
+    if (!user || !text ) {
+      return;
+    }
+
+    this.props.onMessageSubmit({user: user, text: text});
+
+    this.refs.user.value = '';
+    this.refs.text.value = '';
+  },
+  render: function () {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Name" ref="user" />
+        <input type="text" placeholder="Message" ref="text" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
